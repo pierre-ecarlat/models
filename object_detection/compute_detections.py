a6c3c494af862fb06578a8d4f5030ca1162e4a67
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2017-07-18 16:10:33 +0900

    attemptto fix raise_memory error

--- object_detection/compute_detections.py
+++ object_detection/compute_detections.py
@@ -186,25 +186,21 @@ ping (['Start compute detections'])
 
 
 with detection_graph.as_default():
+  image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+  boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+  scores = detection_graph.get_tensor_by_name('detection_scores:0')
+  classes = detection_graph.get_tensor_by_name('detection_classes:0')
+  num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
   with tf.Session(graph=detection_graph, config=tfConfig) as sess:
     for image_idx, image_path in enumerate(TEST_IMAGE_PATHS):
       displayProgress (image_idx, len(TEST_IMAGE_PATHS), 1, image_path)
 
       image = Image.open(image_path)
       width, height = image.size
-      # the array based representation of the image will be used later in order to prepare the
-      # result image with boxes and labels on it.
       image_np = load_image_into_numpy_array(image)
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
       
       # Actual detection.
       (boxes, scores, classes, num_detections) = sess.run(
